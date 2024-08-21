27d8986d094b4383697d57833c01e11299241bef
zohidbe <zohaxaydarov@gmail.com>
2024-08-21 12:51:58 +0500

    hom workd end

---
+++ app.js
@@ -0,0 +1,52 @@
+// task 1
+
+// const sumOfDigits = (sum) => {
+//     return Number.toString().split("").reduce((sum, digit) => sum + Number(digit), 0) 
+// };
+
+// let number = prompt("son kiriting")
+// console.log("raqal yigindisi: ", sumOfDigits(number));
+
+// task 2 
+
+// const sumArrayDigits = (...Number) => {
+//     let sum = Numbers.reduce((acc, num ) => acc + num, 0);
+//     return sum.toString().split("").map(number);
+// };
+
+// console.log("Natija :", sumArrayDigits(1, 22 ,3));
+
+// task 3 
+
+// const isPalindrome = (num) => {
+//     let str = num .toString();
+//     return str === str.split('').reverse().join('');
+// };
+
+// let number = prompt("Son kiriting:");
+// let squared = number ** 2;
+// console.log(`${squared} soni palindrome ${isPalindrome(squared) ? 'ha' : 'yo\'q'}`);
+
+// task 4
+
+// const sumOfDigits = () => {
+// let randomNum = Math.floor(Math.random() * 1000);
+// console.log("Tasodifiy raqam :" , randomNum);
+// return randomNum.toString().split('').reduce((sum, digit) => sum + Number(digit), 0);
+// };
+
+// const  callbacFunction = (callback) =>  {
+//     let result = callback();
+//     console.log("raqam lar yigindisi:", result );
+// };
+
+// callbacFunction(sumOfDigits);
+
+// task 5
+
+// const isWordpalidrome = (word) => {
+//     return word  === word.split('').reverse().join('');
+// };
+
+// let word = prompt("Soz kiriting ");
+// console.log(`${word} sozi palindrome ${isWordpalidrome(word) ?  'ha' : 'yo\q'}`);
